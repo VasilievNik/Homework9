3bd2a344167092c3334ae933fe91e60131310b78
nikit <nikita.veselyev@gmail.com>
2022-05-14 14:24:16 +0300

    1st commit

---
+++ src/com/company/Author.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Author {
+    String Name;
+    String Sername;
+    public Author(String Name, String Sername){
+        this.Name = Name;
+        this.Sername = Sername;
+    }
+
+    public String getAuthorName() {
+        return this.Name;
+    }
+    public String getAuthorSername() {
+        return this.Sername;
+    }
+}
