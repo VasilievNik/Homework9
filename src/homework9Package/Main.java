4a0e460ef8691777dfce5db1b4d0de94a13e0b13
nikit <nikita.veselyev@gmail.com>
2022-05-15 21:17:57 +0300

    1st commit

--- src/homework9Package/Main.java
+++ src/homework9Package/Main.java
@@ -2,17 +2,4 @@ package homework9Package;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Author author1 = new Author("Gneg", "Johnson");
-        Book book1 = new Book(1999, "Life and living", author1);
-
-        Author author2 = new Author("Jimmy", "Smith");
-        Book book2 = new Book(2023, "I hate my computer", author2);
-
-        book1.getBookAllInfo();
-        book2.getBookAllInfo();
-        book2.setPublishDate(2021);
-        book2.getBookAllInfo();
-        //Gneg
-    }
 }
